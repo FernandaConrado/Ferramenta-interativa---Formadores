8f71018efc04fc187e5cc59361e56498b685cc20
FernandaConrado <168746707+FernandaConrado@users.noreply.github.com>
2024-11-28 18:17:06 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Charada',
+    'O que é o que é: Anda mas não sai do lugar?',
+    'Relógio'
+)
+
+criaCartao(
+    'Charada',
+    'O que é o que é: Tem cama, mas não dorme?',
+    'Rio'
+)
+
+criaCartao(
+    'Charada',
+    'O que é o que é: sobe, mas nunca desce?',
+    'A Idade'
+)
+
+criaCartao(
+    'Charada',
+    'O que é o que é: pode ser pequeno, mas nunca se perde, sempre está com você?',
+    'Nome'
+)
